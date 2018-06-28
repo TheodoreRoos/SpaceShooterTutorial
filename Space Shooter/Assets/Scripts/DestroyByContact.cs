7bb6fa25104016d00d69d3733843d64f423f2c0d
Théodore Roos <fjodde98@gmail.com>
2018-06-28 10:40:30 +0200

    Added audio, and builded the game

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour 
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
+        {
+            return;
+        }
+
+        if(explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, transform.position, transform.rotation);
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
